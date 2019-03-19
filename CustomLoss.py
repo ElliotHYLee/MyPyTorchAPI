fc0e2ffaa5be4643a947c5f7255e006ce4624a65
Hongyun Lee (Elliot) <hylee101001@gmail.com>
2019-03-19 15:52:22 -0400

    asdf

--- CustomLoss.py
+++ CustomLoss.py
@@ -48,3 +48,31 @@ class MahalanobisLoss(torch.nn.Module):
     def EuclideanLoss(self, pr_x, x):
         error = torch.abs(pr_x - x)
         return torch.mean(error)
+
+if __name__ == '__main__':
+    import numpy as np
+    x = np.array([[1, 2, 3, 4, 5, 6],
+                  [2, 3, 4, 5, 6, 7]], dtype=np.float32)
+    x = torch.from_numpy(x)
+    L = torch.zeros(2, 3, 3, dtype=torch.float)
+    LT = torch.zeros(2, 3, 3, dtype=torch.float)
+    index = 0
+    for j in range(0, 3):
+        for i in range(0, j + 1):
+            L[:, j, i] = x[:, index]
+            LT[:, i, j] = x[:, index]
+            index += 1
+
+    print(x)
+    print(L)
+    Q = torch.matmul(L, LT)
+    invQ = torch.inverse(Q)
+
+    error = torch.from_numpy(np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float32))
+    dd = error.unsqueeze(1).matmul(invQ)
+    dd = torch.matmul(dd, error.unsqueeze(2))
+    dd = dd.squeeze(2)
+    print(dd)
+
+    lin = Q.reshape(-1, 9)
+    lin = torch.sum(lin, dim=1).unsqueeze(1)
